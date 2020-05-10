6e802ec3be1bd2c08108d7c70fcbcdede78334a9
Tim Addison <git@tjaddison.com>
2020-05-10 13:41:26 -0400

    revert hello api changes

--- tailwind/pages/api/hello.ts
+++ tailwind/pages/api/hello.ts
@@ -2,9 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  // let's violate some rules
-  const x = 0;
-
   res.statusCode = 200;
   res.json({ name: 'John Doe Typescript III' });
 };
